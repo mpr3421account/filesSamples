b3aa56e03bbcf3bd37f3f9bd9eb7a3a5509483f8
Marcos Paulo <itsolutions031250@gmail.com>
2022-10-18 09:46:48 -0300

    WriteFiles

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,25 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        String[] lines = new String[] {"Good Morning", "Good afternoon", "Good night"};
 
-        String path = "c:\\testeJava\\in.txt";
+        String path = "C:\\testeJava\\out.txt";
 
-        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+            for(String line : lines){
+                bw.write(line);
+                bw.newLine();
 
-            String line = br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
             }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
         }
     }
 }
